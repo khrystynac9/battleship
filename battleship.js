30d114c2245d6e4dbc52430a9a3684d0a960146d
Khrystyna <khrystynkakitty@gmail.com>
2017-01-24 20:44:41 +0000

    text

---
+++ battleship.js
@@ -0,0 +1,31 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready? fire! (Enter a number 0 - 6):");
+    if (guess < 0 || guess >6) {
+        alert('Please, enter valid number!');
+    } else {
+        guesses = guesses +1;
+    
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT");
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You won!!!");
+            } 
+        } else {
+            alert("MISS");
+        }
+    }    
+}
+var stats = "You took " + guesses + " guesses to sink the ship, " +
+            "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
