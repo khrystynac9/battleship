69d2a1a740efd6663a180ac888a785f86f42645e
Khrystyna <khrystynkakitty@gmail.com>
2017-01-25 14:08:00 +0000

    text

--- battleship.js
+++ battleship.js
@@ -1,10 +1,13 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
+
+console.log(randomLoc);
 
 while (isSunk == false) {
     guess = prompt("Ready? fire! (Enter a number 0 - 6):");
